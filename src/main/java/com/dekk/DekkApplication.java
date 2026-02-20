d3bfe068bf001fb62394b8d6862299b71420b7d5
youable <youable.framios@gmail.com>
2026-02-20 22:38:11 +0900

    init project

---
+++ src/main/java/com/dekk/DekkApplication.java
@@ -0,0 +1,13 @@
+package com.dekk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DekkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DekkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dekk/DekkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dekk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DekkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
