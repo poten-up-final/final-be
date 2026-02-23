429975609350c349c5aba2123aa36afdf87dee7c
youable <115457023+youabledev@users.noreply.github.com>
2026-02-23 14:11:12 +0900

    hotfix(common): ErrorResponse에서 status 제거 (DK-35)

--- src/main/java/com/dekk/common/error/ErrorResponse.java
+++ src/main/java/com/dekk/common/error/ErrorResponse.java
@@ -3,11 +3,9 @@ package com.dekk.common.error;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.List;
-import org.springframework.http.HttpStatus;
 
 @JsonInclude(Include.NON_NULL)
 public record ErrorResponse(
-    HttpStatus status,
     String code,
     String message,
     List<String> errors
@@ -18,7 +16,6 @@ public record ErrorResponse(
 
     public static ErrorResponse of(ErrorCode errorCode, List<String> errors) {
         return new ErrorResponse(
-            errorCode.status(),
             errorCode.code(),
             errorCode.message(),
             errors
