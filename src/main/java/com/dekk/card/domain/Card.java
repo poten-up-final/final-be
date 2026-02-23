0e8a69dd0b10170c3b24e702f86fdcef214a477b
youable <youable.framios@gmail.com>
2026-02-23 14:49:18 +0900

    refactor(card): 불필요한 애노테이션 제거

--- src/main/java/com/dekk/card/domain/Card.java
+++ src/main/java/com/dekk/card/domain/Card.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Comment;
 
 @Table(name = "cards")
 @Entity
