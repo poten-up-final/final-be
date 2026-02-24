ee8967d9fcc3e5357681925efb211f1e8772416b
youable <youable.framios@gmail.com>
2026-02-24 12:32:33 +0900

    fix(card): 에러 코드 컨벤션 정의

--- src/main/java/com/dekk/card/domain/exception/CardErrorCode.java
+++ src/main/java/com/dekk/card/domain/exception/CardErrorCode.java
@@ -4,14 +4,14 @@ import com.dekk.common.error.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum CardErrorCode implements ErrorCode {
-    PRODUCT_RAW_DATA_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC001", "상품 원본 데이터는 필수 값입니다"),
-    PRODUCT_ORIGIN_URL_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC002", "상품 원본 이미지 경로는 필수 값입니다"),
-    PRODUCT_NAME_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC003", "상품 이름은 필수값입니다"),
-    PRODUCT_EXTERNAL_ID_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC004", "상품 고유 id는 필수값입니다"),
-    CARD_RAW_DATA_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC005", "카드 원본 데이터는 필수 값입니다"),
-    CARD_ORIGIN_URL_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC006", "카드 원본 이미지 경로는 필수 값입니다"),
-    CARD_ORIGIN_ID_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC007", "카드 고유 id는 필수 값입니다"),
-    CARD_PLATFORM_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC008", "카드 플랫폼은 필수 값입니다"),
+    PRODUCT_RAW_DATA_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC40001", "상품 원본 데이터는 필수 값입니다"),
+    PRODUCT_ORIGIN_URL_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC40002", "상품 원본 이미지 경로는 필수 값입니다"),
+    PRODUCT_NAME_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC40003", "상품 이름은 필수값입니다"),
+    PRODUCT_EXTERNAL_ID_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC40004", "상품 고유 id는 필수값입니다"),
+    CARD_RAW_DATA_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC40005", "카드 원본 데이터는 필수 값입니다"),
+    CARD_ORIGIN_URL_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC40006", "카드 원본 이미지 경로는 필수 값입니다"),
+    CARD_ORIGIN_ID_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC40007", "카드 고유 id는 필수 값입니다"),
+    CARD_PLATFORM_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EC40008", "카드 플랫폼은 필수 값입니다"),
     ;
 
     private final HttpStatus httpStatus;
