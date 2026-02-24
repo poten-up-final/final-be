d38b7d4ec7f3d160089ba0f04de44cb7e48ed762
woongblack <woongblack123@gmail.com>
2026-02-25 04:26:58 +0900

    feat(auth): OAuth2 소셜 로그인 서비스 구현

---
+++ src/main/java/com/dekk/auth/application/CustomOAuth2UserService.java
@@ -0,0 +1,59 @@
+package com.dekk.auth.application;
+
+import com.dekk.security.oauth2.exception.AuthErrorCode;
+import com.dekk.user.application.command.UserCreateCommand;
+import com.dekk.user.domain.model.User;
+import com.dekk.user.domain.model.enums.Provider;
+import com.dekk.user.domain.repository.UserRepository;
+import com.dekk.security.oauth2.CustomUserDetails;
+import com.dekk.security.oauth2.OAuth2UserInfoFactory;
+import com.dekk.security.oauth2.dto.OAuth2UserInfo;
+import com.dekk.security.oauth2.exception.CustomOAuth2Exception;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        Provider provider;
+        try {
+            provider = Provider.valueOf(registrationId.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new CustomOAuth2Exception(AuthErrorCode.UNSUPPORTED_PROVIDER);
+        }
+
+
+        OAuth2UserInfo userInfo = OAuth2UserInfoFactory.of(provider, attributes);
+
+        User user = saveOrUpdate(userInfo, provider);
+
+        return new CustomUserDetails(user, attributes);
+    }
+
+    private User saveOrUpdate(OAuth2UserInfo userInfo, Provider provider) {
+        return userRepository.findByProviderAndProviderId(provider, userInfo.getProviderId())
+                .orElseGet(() -> userRepository.save(
+                        User.create(new UserCreateCommand(userInfo.getEmail(), provider, userInfo.getProviderId()))
+                ));
+
+    }
+}
