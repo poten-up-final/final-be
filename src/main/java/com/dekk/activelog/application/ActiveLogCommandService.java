a9ccdf64015d14152c542a15f960ff26d6979f22
Sung Ryul Cho <hahahoho5811@gmail.com>
2026-02-26 20:13:58 +0900

    feat(activeLog) : 스와이프 이력 저장 비즈니스 로직 구현

---
+++ src/main/java/com/dekk/activelog/application/ActiveLogCommandService.java
@@ -0,0 +1,28 @@
+// 📂 application/ActiveLogCommandService.java
+package com.dekk.activelog.application;
+
+import com.dekk.activelog.application.dto.command.SwipeCommand;
+import com.dekk.activelog.domain.exception.ActiveLogBusinessException;
+import com.dekk.activelog.domain.exception.ActiveLogErrorCode;
+import com.dekk.activelog.domain.model.ActiveLog;
+import com.dekk.activelog.domain.repository.ActiveLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ActiveLogCommandService {
+
+    private final ActiveLogRepository activeLogRepository;
+
+    @Transactional
+    public void saveSwipeAction(SwipeCommand command) {
+        if (activeLogRepository.existsByUserIdAndCardId(command.userId(), command.cardId())) {
+            throw new ActiveLogBusinessException(ActiveLogErrorCode.ALREADY_SWIPED);
+        }
+
+        ActiveLog activeLog = ActiveLog.create(command.userId(), command.cardId(), command.swipeType());
+        activeLogRepository.save(activeLog);
+    }
+}
