344946b49f8d8378344e6556d93a4279587a921f
woongblack <woongblack123@gmail.com>
2026-02-27 00:25:34 +0900

    feat(security): JWT 인증 필터 구현

---
+++ src/main/java/com/dekk/security/jwt/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.dekk.security.jwt.filter;
+
+
+import com.dekk.security.jwt.JwtTokenProvider;
+import com.dekk.security.jwt.exception.JwtBusinessException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String jwt = resolveToken(request);
+
+        if (StringUtils.hasText(jwt)) {
+            try {
+                if (jwtTokenProvider.validateToken(jwt)) {
+                    Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), request.getRequestURI());
+                }
+            } catch (JwtBusinessException e) {
+                log.debug("JWT 인증 실패: {}, uri: {}", e.getMessage(), request.getRequestURI());
+            }
+        } else {
+            log.debug("유효한 JWT 토큰이 없습니다, uri: {}", request.getRequestURI());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
