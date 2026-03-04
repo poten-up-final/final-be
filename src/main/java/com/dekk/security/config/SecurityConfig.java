ff61cee24e1d464f49e6892f4c8ca57ec5873315
woongblack <woongblack123@gmail.com>
2026-03-05 04:55:17 +0900

    feat(security): 토큰 재발급 엔드포인트 permitAll 추가

--- src/main/java/com/dekk/security/config/SecurityConfig.java
+++ src/main/java/com/dekk/security/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
                                 "/v3/api-docs",
+                                "/w/v1/auth/refresh",
                                 "/w/v1/cards",
                                 "/w/v1/cards/*/swipe",
                                 "/w/v1/guests/cards/**",
