2b56956ad5d57335fff3098de93883d68971b501
youable <youable.framios@gmail.com>
2026-03-08 18:13:59 +0900

    feat(category): category controller layer 구현

---
+++ src/main/java/com/dekk/category/presentation/response/CategoryResultCode.java
@@ -0,0 +1,24 @@
+package com.dekk.category.presentation.response;
+
+import com.dekk.common.response.ResultCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum CategoryResultCode implements ResultCode {
+    CATEGORY_CREATED(HttpStatus.CREATED, "SCT20101", "카테고리 생성 성공")
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    @Override
+    public HttpStatus status() { return status; }
+
+    @Override
+    public String code() { return code; }
+
+    @Override
+    public String message() { return message; }
+}
